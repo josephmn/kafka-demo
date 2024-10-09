80acd5d99a1f296b6963d2b8df49a261383b95a0
Joseph Magallanes <josephcarlos.jcmn@gmail.com>
2024-10-08 20:46:02 -0500

    config topic kafka

--- src/main/java/api/azure/kafka_demo/config/KafkaTopicConfig.java
+++ src/main/java/api/azure/kafka_demo/config/KafkaTopicConfig.java
@@ -1,4 +1,19 @@
 package api.azure.kafka_demo.config;
 
-public class kafkaTopicConfig {
+import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.TopicBuilder;
+
+@Configuration
+public class KafkaTopicConfig {
+
+    @Bean
+    public NewTopic exampleMessageTopic() {
+        return TopicBuilder
+                .name("example-message-topic")
+//                .partitions(5)
+//                .replicas(2)
+                .build();
+    }
 }

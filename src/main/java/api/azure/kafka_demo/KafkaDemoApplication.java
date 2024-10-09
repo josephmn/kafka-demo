52c9ba2541405bb43f1221a91ff6b7d2bf592774
Joseph Magallanes <josephcarlos.jcmn@gmail.com>
2024-10-08 19:32:21 -0500

    first commit

---
+++ src/main/java/api/azure/kafka_demo/KafkaDemoApplication.java
@@ -0,0 +1,13 @@
+package api.azure.kafka_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/api/azure/kafka_demo/KafkaDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package api.azure.kafka_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

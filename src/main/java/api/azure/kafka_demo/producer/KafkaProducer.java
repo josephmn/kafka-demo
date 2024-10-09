65529a6f02649f69890a0634af4a50570f0b9916
Joseph Magallanes <josephcarlos.jcmn@gmail.com>
2024-10-08 20:48:46 -0500

    Creating and configuring the producer

--- src/main/java/api/azure/kafka_demo/producer/KafkaProducer.java
+++ src/main/java/api/azure/kafka_demo/producer/KafkaProducer.java
@@ -1,4 +1,19 @@
 package api.azure.kafka_demo.producer;
 
-public class kafkaProducer {
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class KafkaProducer {
+
+    private final KafkaTemplate<String, String> kafkaTemplate;
+
+    public void sendMessage(String msg) {
+        log.info("Sending message to topic -> : example-message-topic :: '{}'", msg);
+        kafkaTemplate.send("example-message-topic", msg);
+    }
 }
--- src/main/java/api/azure/kafka_demo/rest/MessageController.java
+++ src/main/java/api/azure/kafka_demo/rest/MessageController.java
@@ -1,4 +1,23 @@
 package api.azure.kafka_demo.rest;
 
+import api.azure.kafka_demo.producer.KafkaProducer;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/messages")
+@RequiredArgsConstructor
 public class MessageController {
+
+    private final KafkaProducer kafkaProducer;
+
+    @PostMapping
+    public ResponseEntity<String> sendMessage(@RequestBody String message) {
+        kafkaProducer.sendMessage(message);
+        return ResponseEntity.ok("Message queued successfully");
+    }
 }
